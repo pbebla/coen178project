916d0f7cf91f22791e406e0af22308831523ea95
sportsman2325 <sportsman2325@yahoo.com>
2019-03-08 17:33:54 -0800

    Add files via upload

---
+++ ProjectValues.sql
@@ -0,0 +1,36 @@
+--Car Table
+INSERT INTO Car VALUES ('A1111','H1234','4081234567');
+INSERT INTO Car VALUES ('A2222','V2346','6712834567');
+
+
+--Customer Table
+INSERT INTO Customers VALUES ('1408123456','Bebla','124 Willow St. Santa Clara, CA 95053');
+INSERT INTO Customers VALUES ('6712834567','Chan','349 Dimple St. Sunnyvale, CA 95053');
+
+
+--Problem Table
+INSERT INTO Problem VALUES (1,'Battery');
+INSERT INTO Problem VALUES (2,'Engine');
+INSERT INTO Problem VALUES (3,'Tires');
+
+
+--Mechanic Table
+INSERT INTO Mechanic VALUES (43,'Adams','4089999999',20.00);
+INSERT INTO Mechanic VALUES (24,'Smith','8887124003',20.00);
+
+
+--RepairJob Table
+
+
+--Parts Table
+INSERT INTO Parts VALUES ('Nut',100.00);
+INSERT INTO Parts VALUES ('Bolt',500.00);
+
+
+--PartsUsed Table
+
+
+
+--ProblemsFixed Table
+
+
