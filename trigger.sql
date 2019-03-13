e166c4460b7efb6cee0e2012696cafd8f8f03f9d
Michelle Yeon <43872057+myeon44@users.noreply.github.com>
2019-03-12 23:35:39 -0700

    Add files via upload

---
+++ trigger.sql
@@ -0,0 +1,21 @@
+CREATE OR Replace TRIGGER createbill 
+AFTER UPDATE ON RepairJob 
+FOR EACH ROW
+
+DECLARE
+r_id integer := :new.repairjobId;
+names varchar2(20);
+pcost decimal;
+
+BEGIN 
+IF :new.time_out<>:old.time_out THEN
+Select pname into names from PartsUsed where repairjobId=r_id;
+Select cost into pcost from Parts where pname=names;
+
+update RepairJob
+set bill = 30 + pcost + (25*:new.laborhrs)
+
+END IF; 
+END;
+/ 
+Show errors;
