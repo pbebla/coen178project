365cb657067b534776e769306727ea69544cb136
Michelle Yeon <43872057+myeon44@users.noreply.github.com>
2019-03-13 19:18:40 -0700

    Add files via upload

---
+++ main.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+      <title>Meteor RepairRecord System</title>
+   </head>
+   <body>
+<header>
+	<h1>Welcome to Meteor RepairRecord System. Select any of the options.</h1>
+</header>
+<form>
+	<input type="button" value="Create Repair Job" onclick="window.location.href='http://linux.students.engr.scu.edu/~myeon/repairjob.php'" />
+</form>
+<br/> 
+<form>
+	<input type="button" value="Generate Customer Bill" onclick="window.location.href='http://linux.students.engr.scu.edu/~myeon/bill.php'" />
+</form>
+<br/>
+<form>
+	<input type="button" value="Show repair jobs between dates" onclick="http://linux.students.engr.scu.edu/~myeon/dates.php'" />
+</form>
+<br/>
+<form>
+	<input type="button" value="Find Mechanic's pay" onclick="window.location.href='http://linux.students.engr.scu.edu/~myeon/pay.php'" />
+</form>
+<br/>
+   </body>
+</html>
