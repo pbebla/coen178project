b100416318776d6212f2a3bc4f6cf359af0b1ed0
andrea-chan <48495522+andrea-chan@users.noreply.github.com>
2019-03-13 19:47:28 -0700

    Add files via upload

---
+++ calcTotalBill.sql
@@ -0,0 +1,18 @@
+-- 1.3(e): function to show amount of money generated from customer billing 
+-- start/end dates given as parameters
+
+Create or Replace Function calcTotalBill (start_date IN TIMESTAMP, end_date IN TIMESTAMP)
+RETURN DECIMAL
+IS
+l_bill DECIMAL := 0.00;					/* individual bill amounts */
+l_total DECIMAL := 0.00;				/* total, initialised to 0 */
+BEGIN
+SELECT bill INTO l_bill FROM RepairJob 
+WHERE time_out >= start_date AND time_out <= end_date;
+l_total := l_total + l_bill;
+
+/* return total value */
+RETURN l_total;
+END;
+/
+Show errors;
