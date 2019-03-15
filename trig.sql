d64a8c450a9380ff86397b62b0592faa608c9c5d
Michelle Yeon <43872057+myeon44@users.noreply.github.com>
2019-03-15 01:09:14 -0700

    Add files via upload

---
+++ trig.sql
@@ -0,0 +1,28 @@
+CREATE OR Replace TRIGGER createbill 
+AFTER UPDATE of time_out ON RepairJob 
+FOR EACH ROW
+
+DECLARE
+pcost decimal;
+r_bill decimal;
+last_service_date TIMESTAMP;
+
+
+BEGIN
+Select sum(cost) into pcost from Parts where pname in (select pname from PartsUsed where repairjobId=:old.repairjobid);
+
+--Select MAX(time_out) into last_service_date from RepairLog inner join Car where RepairLog.car_license_no=Car.car_license_no;
+
+IF (last_service_date > (:new.time_out - 365)) then
+	r_bill := 0.9*(30 + pcost + (25*:old.laborhrs));
+ELSE
+	r_bill := 30 + pcost + (25*:old.laborhrs);
+END IF;  
+
+insert into repairlog values (:old.repairjobid,:old.car_license_no,:old.time_in,:new.time_out,:old.emp_id,:old.laborhrs,r_bill);
+
+dbms_output.put_line('The Bill is: '||r_bill);
+
+END;
+/ 
+Show errors;
