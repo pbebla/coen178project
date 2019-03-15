624f3ac7ffdb2c4227800caefebb2cb197782159
sportsman2325 <sportsman2325@yahoo.com>
2019-03-15 11:51:52 -0700

    Add files via upload

--- project_13e.sql
+++ project_13e.sql
@@ -5,23 +5,23 @@
 Create or Replace Function calcTotalBill (start_date IN TIMESTAMP, end_date IN TIMESTAMP)
 RETURN DECIMAL
 IS
-l_bill DECIMAL := 0.00;					/* individual bill amounts */
-l_total DECIMAL := 0.00;				/* total, initialised to 0 */
-CURSOR Log_cur IS
-(Select bill FROM RepairLog
+	l_bill DECIMAL;				/* individual bill amounts */
+	l_total DECIMAL := 0.00;				/* total, initialised to 0 */
+	CURSOR Log_cur IS
+	(Select bill FROM RepairLog
 WHERE time_out >= start_date AND time_out <= end_date);
 
 BEGIN
 -- NEED TO LOOP!!! vvv
-OPEN Log_cur;
-LOOP
-FETCH Log_cur INTO l_bill;
-	EXIT when Log_cur%notfound;
-	l_total := l_total + l_bill;
-END loop;
-CLOSE Log_cur;
-/* return total value */
-RETURN l_total;
+	OPEN Log_cur;
+	LOOP
+	FETCH Log_cur INTO l_bill;
+		EXIT when Log_cur%notfound;
+		l_total := l_total + l_bill;
+	END loop;
+	CLOSE Log_cur;
+	/* return total value */
+	RETURN l_total;
 END;
 /
 Show errors;
